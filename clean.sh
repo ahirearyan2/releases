1d0fcb803c1d45c097d2b79042f3c2cca5af8094
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2021-01-25 11:10:23 +0530

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -5,9 +5,6 @@ export my_dir=$(pwd)
 echo "Loading configuration..."
 source "${my_dir}"/config.sh
 
-if [ -z "${GITHUB_TOKEN}" ]; then
-    echo "Please set GITHUB_TOKEN before continuing."
-    exit 1
 fi
 
 # Email for git
